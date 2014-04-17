660f9cc7985c765a6552a1efbaab7fa723813342
thinksmartdares <thinksmartdare@yahoo.com>
2014-04-17 12:19:41 +0530

    Day 2
    one mor epackage

---
+++ src/com/sample/home/SampleTwo.java
@@ -0,0 +1,11 @@
+package com.sample.home;
+
+public class SampleTwo {
+
+	public static void main(String[] args) {
+		System.out.println("-----------------Starts-----------");
+		System.out.println(" This is sample two progam");
+		System.out.println("---------------ends-----------------");
+	}
+
+}
