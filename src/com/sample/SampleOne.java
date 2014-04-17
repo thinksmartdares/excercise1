03ea85982d6c0a33357e798857ceaee0f99afca1
thinksmartdares <thinksmartdare@yahoo.com>
2014-04-17 11:55:10 +0530

    Day One
    Simple Helloworld program

---
+++ src/com/sample/SampleOne.java
@@ -0,0 +1,12 @@
+package com.sample;
+
+public class SampleOne {
+
+	public static void main(String[] args) {
+		System.out.println("----------Program starts---------------");
+		System.out.println("welcome to java worlds");
+		System.out.println("------------Program ends-----------------");
+
+	}
+
+}
